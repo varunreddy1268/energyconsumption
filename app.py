21455d79af6a760828a2efc30a396d128d36664f
varun reddy <71369489+varunreddy1268@users.noreply.github.com>
2024-04-04 13:05:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@
 from flask import Flask, render_template, request, Markup
 import numpy as np
 import pandas as pd
-#from utils.disease import disease_dic
-#from utils.fertilizer import fertilizer_dic
-#from utils.crop_rec import crop_dic
+
 import requests
 import config
 import pickle
@@ -13,10 +11,9 @@ import io
 import torch
 from torchvision import transforms
 from PIL import Image
-import pre_processing as puka
-import model_building as mahesh_babu
-#from utils.model import ResNet9
-#import cure
+import pre_processing as pre_process
+import model_building as build
+
 # ==============================================================================================
 # ===============================================================================================
 # ------------------------------------ FLASK APP -------------------------------------------------
@@ -33,7 +30,7 @@ def home():
     return render_template('index.html', title=title)
 import pandas as pd
 
-@app.route('/disease-predict', methods=['GET', 'POST'])
+@app.route('/do-predict', methods=['GET', 'POST'])
 def disease_prediction():
     title = 'Household Electric Power Consumption'
 
@@ -45,10 +42,10 @@ def disease_prediction():
             return render_template('disease.html', title=title)
         #try:
         df = pd.read_csv(file)
-        x=puka.preprocessing(df,"active_energy",100,2,2)
+        x=pre_process.preprocessing(df,"active_energy",100,2,2)
         train,target1,test,target=x.supreme_method()
 
-        x1=mahesh_babu.model_building((120,1),2)
+        x1=build.model_building((120,1),2)
         final_model=x1.create_model()
         final_model.compile(optimizer="adam",loss="mean_squared_error")
         #print(type(final_model))
